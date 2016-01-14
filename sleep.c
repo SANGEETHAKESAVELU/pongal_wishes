0ad0a084e84c599408fbc02a9d44dede4780c774
SANGEETHAKESAVELU <sangitweetty@gmail.com>
2016-01-14 07:29:46 -0800

    Create sleep.c

---
+++ sleep.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<unistd.h>
+#include<Windows.h>
+int main()
+{
+  int i;
+  char a[12]={"HAPPY PONGAL"};
+  for(i=0;i<=11;i++)
+  {
+    Sleep(1000);
+    printf("%c",a[i]);
+  }
+  return 0;
+}
